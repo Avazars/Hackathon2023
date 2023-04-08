f1dc1e6124883b8235b778193051c5dd4295ad72
jsadler <jadin.sadler@wsu.edu>
2023-04-07 20:37:31 -0700

    got chat to talk to me

--- index.ts
+++ index.ts
@@ -5,6 +5,25 @@ const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'))
+require('dotenv').config()
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function start(){
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content: "Hello world"}],
+    });
+
+    console.log(completion.data.choices[0].message);
+}
+
+start();
 
 app.use('/', mainRouter);
 
